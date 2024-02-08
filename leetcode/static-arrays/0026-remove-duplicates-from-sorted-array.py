7b1cc4243b5c252456c0a889e5edded8b6bc4fdd
Rafael Lopes <rafaelsilveriodesalopes@gmail.com>
2024-02-08 20:57:12 -0300

    feat: create 0026-remove-duplicates-from-sorted-array.py

---
+++ leetcode/static-arrays/0026-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        actualNumber = nums[0]
+        actualPosition = 1
+        k = 1
+
+        for i, n in enumerate(nums):
+            if n != actualNumber:
+                nums[actualPosition] = n
+                actualNumber = n
+                actualPosition += 1
+                k += 1
+
+        return k
