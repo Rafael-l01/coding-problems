b9b2d64697e4deda316d1d6d1e42534f6069a405
Rafael Lopes <rafaelsilveriodesalopes@gmail.com>
2024-02-08 21:16:43 -0300

    refactor: remove k variable, as actualPosition and k are the same variable basically

--- leetcode/static-arrays/0027-remove-element.py
+++ leetcode/static-arrays/0027-remove-element.py
@@ -1,11 +1,9 @@
 class Solution(object):
     def removeElement(self, nums, val):
         actualPosition = 0
-        k = 0
         for n in nums:
             if n != val:
                 nums[actualPosition] = n
                 actualPosition += 1
-                k += 1
 
-        return k
+        return actualPosition
