98fbe6ee0cd2ffa49ae32d6cd338a72f3d0f885a
Rafael Lopes <rafaelsilveriodesalopes@gmail.com>
2024-02-11 18:35:57 -0300

    refactor: remove k variable, as actualPosition and k are the same variable basically

--- leetcode/static-arrays/0026-remove-duplicates-from-sorted-array.py
+++ leetcode/static-arrays/0026-remove-duplicates-from-sorted-array.py
@@ -2,13 +2,11 @@ class Solution(object):
     def removeDuplicates(self, nums):
         actualNumber = nums[0]
         actualPosition = 1
-        k = 1
 
         for i, n in enumerate(nums):
             if n != actualNumber:
                 nums[actualPosition] = n
                 actualNumber = n
                 actualPosition += 1
-                k += 1
 
-        return k
+        return actualPosition
