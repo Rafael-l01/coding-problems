71f3b63dae25fdb08ce615b67161054d2a1b414a
Rafael Lopes <rafaelsilveriodesalopes@gmail.com>
2024-03-04 20:49:46 -0300

    feat: solve leetcode problem 0217-contains-duplicate

---
+++ leetcode/hash-tables/0217-contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hashSet = set()
+        for num in nums:
+            if num in hashSet:
+                return True
+
+            hashSet.add(num)
+
+        return False
