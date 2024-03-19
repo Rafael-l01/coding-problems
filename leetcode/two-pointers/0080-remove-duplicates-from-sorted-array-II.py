26cd8f3f28d3a639ff34115b9c7f03e5e0fbdad3
Rafael Lopes <rafaelsilveriodesalopes@gmail.com>
2024-03-19 20:34:07 -0300

    fix: move solutions of remove 0080-remove-duplicates problem that were wrongly in 0167-two-sum-II solution

--- leetcode/two-pointers/0080-remove-duplicates-from-sorted-array-II.py
+++ leetcode/two-pointers/0080-remove-duplicates-from-sorted-array-II.py
@@ -1,6 +1,7 @@
 from typing import List
 
 
+# first solution
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         actualNumber = nums[0]
@@ -20,3 +21,49 @@ class Solution:
                 nextPosition += 1
 
         return nextPosition
+
+
+# second solution
+class Solution2:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        left = 0
+        right = 0
+        actualNumber = nums[left]
+
+        while right < len(nums):
+            counter = 0
+            while right < len(nums) and nums[right] == actualNumber:
+                if counter < 2:
+                    counter += 1
+                right += 1
+
+            for i in range(counter):
+                nums[left] = actualNumber
+                left += 1
+
+            if right < len(nums):
+                actualNumber = nums[right]
+
+        return left
+
+
+# third solution
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        left = 0
+        right = 0
+
+        while right < len(nums):
+            counter = 1
+            while right + 1 < len(nums) and nums[right] == nums[right + 1]:
+                if counter < 2:
+                    counter += 1
+                right += 1
+
+            for i in range(counter):
+                nums[left] = nums[right]
+                left += 1
+
+            right += 1
+
+        return left
